e631941af3d3aba5366c1ee001abde721e938289
Tarishij <tarishijain17@gmail.com>
2018-03-03 14:14:54 +0530

    Create NumberOps.h

---
+++ lab2/task2/NumberOps.h
@@ -0,0 +1,6 @@
+struct Number createNumber(int,char *);
+struct Number add(struct Number,struct Number);
+struct Number subtract(struct Number,struct Number);
+struct Number convert(int,int);
+void printNumber(struct Number);
+int convert_in_decimal(struct Number);
