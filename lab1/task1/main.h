027b4fbbfd77778621efe7c5ea96f0c7106bcbc7
Tarishij <tarishijain17@gmail.com>
2018-03-03 14:34:21 +0530

    Create main.h

---
+++ lab1/task1/main.h
@@ -0,0 +1,7 @@
+void insertion(int[],int);
+void merge(int[],int,int,int);
+void merge_sort(int[],int,int);
+void bubble(int a[],int n);
+void selection(int[],int);
+void quick(int[],int,int);
+int partition(int[],int,int);
